3572542353c64f451bfae9294877bdcd2d27ad5e
James Risberg <jimmyrisberg56@gmail.com>
2016-01-13 14:44:46 -0500

    Display listings

--- listingController.js
+++ listingController.js
@@ -1,14 +1,18 @@
 angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
   function($scope, Listings) {
     $scope.listings = Listings;
-    $scope.detailedInfo = undefined;
+    $scope.currentListing = $scope.listings[0];
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
-     */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+        
+    };
+      
+    $scope.deleteListing = function(index) {
+        $scope.listings.splice(index, 1);
+    };
+      
+    $scope.showDetails = function(index) {
+        $scope.currentListing = $scope.listings[index];
+    };
   }
 ]);
